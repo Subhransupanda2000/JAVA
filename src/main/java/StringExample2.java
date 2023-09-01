6e449113a538ed49a502e0f2e19949daa4ce606d
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-09-01 16:20:51 +0530

    String Compare

---
+++ src/main/java/StringExample2.java
@@ -0,0 +1,17 @@
+public class StringExample2 {
+    public static void main(String[] args) {
+        String name1 = "Tony";
+        String name2 = "Tony";
+
+
+        if (name1.equals(name2)) {
+            System.out.println("equals");
+        } else {
+            System.out.println("not equals");
+        }
+        
+
+
+    }
+}
+
