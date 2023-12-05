9d088de777b4e9bc3ca048e90d0e26aa6cabdced
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-12-05 19:21:21 +0530

    Default Constructer

---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+    int age;
+    String country;
+
+    public Cat() {
+        this.age = 25;
+        this.country = "india";
+        System.out.println(age);
+        System.out.println(country);
+    }
+
+    public static void main(String[] args) {
+        Cat c = new Cat();
+
+
+    }
+}
