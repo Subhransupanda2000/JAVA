cc15fed96e178c9c8d687926db54c1182dfc6755
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-02-12 11:41:55 +0530

    Default method

---
+++ src/main/java/Interff.java
@@ -0,0 +1,19 @@
+public interface Interff {
+    String m2();
+    default String m1()
+    {
+        return "hello";
+    }
+}
+    class Testing implements Interff {
+        @Override
+        public String m2() {
+            return "hii";
+        }
+        public static void main(String[] args) {
+            Testing t1 = new Testing();
+            t1.m1();
+            t1.m2();
+        }
+
+    }
